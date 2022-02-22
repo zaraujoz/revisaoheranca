143928df1b166537c873408ec13089df382ab647
zaraujoz <matheusha@live.com>
2022-02-22 17:33:32 +0000

    Adicionando Arquivos

---
+++ Pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa{
+
+    private String nome;
+    private String endereço;
+    private String telefone;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getEndereço() {
+        return endereço;
+    }
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
+    
